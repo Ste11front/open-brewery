257a506dc61837941ecf9c0546ad4f53dda65eac
Ste11front <Ste11front@users.noreply.github.com>
2023-07-13 22:33:31 +0200

    inizio

--- main.js
+++ main.js
@@ -1,7 +1,31 @@
-DBService.getBreweries()
-.then(breweries => render (breweries));
 
-function render(breweries) {
-    
-    for (breweries)
+let pageNumber = 1;
+
+DBService.getBreweries(pageNumber)
+.then(breweries => render(breweries))
+
+function render(breweries){
+
+    for (brewery of breweries) {
+        console.log(brewery.name)
+        
+    }
+
 }
+
+
+function previous() {
+    pageNumber--;
+    DBService.getBreweries(pageNumber)
+    .then(breweries => render(breweries))
+}
+
+function next(){
+    pageNumber++;
+    DBService.getBreweries(pageNumber)
+    .then(breweries => render(breweries))
+
+}
+
+
+//displaiare le informazioni delle birrerie nella maniera più cool possibile
--- services/db-services.js
+++ services/db-services.js
@@ -1,7 +1,14 @@
 class DBService{
-    static getBreweries(){
-        const url = 'https://api.openbrewerydb.org/v1/breweries';
-        return fetch<(url)
+
+    static getBreweries(pageNumber){
+
+        const url = 'https://api.openbrewerydb.org/v1/breweries?per_page=10&page=' + pageNumber;
+
+        console.log(url);
+
+        return fetch(url)
         .then(resp => resp.json())
+
     }
+
 }
